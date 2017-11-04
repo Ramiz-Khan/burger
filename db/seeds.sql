32344a4540ea3ad7d20bb7b4e1ed2addc0caf5e0
Ramiz khan <ramizkhan@Ramizs-MacBook-Pro-2.local>
2017-11-03 20:08:10 -0500

    sql connected

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers_db.burgers (burger_name, devoured, date) VALUES  ('bacon', true,  '11-1-17 00:00:00');
+
+
+INSERT INTO burgers_db.burgers (burger_name, devoured, date) VALUES  ('green', false,  '07-5-17 00:00:00');
+
+
+INSERT INTO burgers_db.burgers (burger_name, devoured, date) VALUES  ('american', true,  '08-4-17 00:00:00');
+
